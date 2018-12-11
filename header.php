942559bec397701f584ed5b4c835bbcce932a63c
helg-s <oj.skvor@gmail.com>
2018-12-11 19:10:46 +0300

    Начало

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Архитектурное бюро "TOWER"</title>                                                                                                 
+</head>
+<body>
+
+	
---
+++ mail.php
@@ -0,0 +1,43 @@
+﻿<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+// преобразует все символы, которые пользователь попытается добавить в форму
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$phone = htmlspecialchars($phone);
+$message = htmlspecialchars($message);
+
+ // декодирует url, если пользователь попытается его добавить в форму
+$name = urldecode($name);
+$email = urldecode($email);
+$phone = urldecode($phone);
+$message = urldecode($message);
+
+ // удалим пробелы с начала и конца строки, если таковые имеются
+$name = trim($name);
+$email = trim($email);
+$phone = trim($phone);
+$message = trim($message);
+// На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+
+
+// echo $name;
+// echo "<br>";
+// echo $email;
+// echo "<br>";
+// echo $phone;
+// echo "<br>";
+// echo $message;
+
+mail("helgus@bk.ru", "Заявка с сайта" , "ФИО:". $name . "E-mail: ". $email . "Телефон:" . $phone . "Сообщение: " . $message);
+
+if (mail("helgus@bk.ru", "Заявка с сайта", "ФИО:". $name . "E-mail: ". $email . "Телефон:" . $phone . "Сообщение: " . $message))
+ {     echo "Cообщение успешно отправлено"; 
+} 
+else { 
+    echo "При отправке сообщения возникли ошибки";
+}
+?>
